75d6de8ee3d2b7c64a39476926ed3b925cec9c00
Arrcival <arrcival@gmail.com>
2022-04-13 16:16:18 +0200

    Added exercises 8-13

--- exercice_list.py
+++ exercice_list.py
@@ -48,3 +48,61 @@ def exercise7(logs, localMethods):
     printValue = s.getvalue().split("\n")[0]
     tester.test("Function tested" == printValue, f"Waiting for : Function tested / Got : {printValue}")
     return tester
+
+def exercise8(logs, localMethods):
+    tester = Tester()
+    firstPrint = logs.split("\n")[0]
+    tester.test("5" == firstPrint, f"Waiting for : 5 / Got : {firstPrint}")
+    return tester
+
+def exercise9(logs, localMethods):
+    tester = Tester()
+    firstPrint = logs.split("\n")[0]
+    tester.test("34348" == firstPrint, f"Waiting for : 34348 / Got : {firstPrint}")
+    return tester
+
+def exercise10(logs, localMethods):
+    tester = Tester()
+    rtr = localMethods["cube"]
+    result1 = rtr.__call__(2)
+    result2 = rtr.__call__(3)
+    result3 = rtr.__call__(5)
+    tester.test("8" == result1, f"Waiting for : 8 / Got : {result1}")
+    tester.test("27" == result2, f"Waiting for : 27 / Got : {result2}")
+    tester.test("125" == result2, f"Waiting for : 125 / Got : {result3}")
+    return tester
+
+def exercise11(logs, localMethods):
+    tester = Tester()
+    rtr = localMethods["printIfTrue"]
+    with stdoutIO() as s:
+        result = rtr.__call__("Should be displayed", True)
+    printValue = s.getvalue().split("\n")[0]
+    tester.test("Should be displayed" == printValue, f"Waiting for : Should be displayed / Got : {printValue}")
+    with stdoutIO() as s:
+        result = rtr.__call__("Should not be displayed", False)
+    printValue = s.getvalue().split("\n")[0]
+    tester.test("" == printValue, f"Waiting for : / Got : {printValue}")
+    return tester
+
+def exercise12(logs, localMethods):
+    tester = Tester()
+    rtr = localMethods["specialMaths"]
+    result = rtr.__call__(10, True)
+    tester.test(11 == result, f"Waiting for : 11/ Got : {result}")
+    result = rtr.__call__(10, False)
+    tester.test(9 == result, f"Waiting for : 9 / Got : {result}")
+    return tester
+
+def exercise13(logs, localMethods):
+    tester = Tester()
+    rtr = localMethods["printIfEquals"]
+    with stdoutIO() as s:
+        result = rtr.__call__(15, 15)
+    printValue = s.getvalue().split("\n")[0]
+    tester.test("They are equals" == printValue, f"Waiting for : They are equals / Got : {printValue}")
+    with stdoutIO() as s:
+        result = rtr.__call__(10, 11)
+    printValue = s.getvalue().split("\n")[0]
+    tester.test("" == printValue, f"Waiting for : / Got : {printValue}")
+    return tester
